07964c6d747563a5bb2f445cc6a6555a23541787
colloso999 <tripperskripper@gmail.com>
2022-08-10 23:39:31 +0300

    Added other source codes

---
+++ Chapter3_ConditionalExecution/OneWayDecisions.py
@@ -0,0 +1,24 @@
+x = 5
+print('Before 5')
+
+#indentation
+
+#indentation error is just another syntax error
+
+#increase indent after an if statement or for statement
+
+#maintain indent to indicate the scope of the block (which lines are affected by the if/for
+
+# Reduce indent back to the level of the if statament to indicate the end of the block)
+
+#comments on a line by themsleves are ignored with regard to indentation 
+if x == 5:
+  print('is 5')
+  print('is still 6')
+print('Afterwards 5')
+print('Before 6')
+if x == 6:
+    print('is 6')
+    print('is still 6')
+    print('Third 6')
+    print('Afterwards 6')
---
+++ Chapter3_ConditionalExecution/OneWayDecisions2.py
@@ -0,0 +1,13 @@
+x = 5
+if x > 2 :
+      print('Bigger than 2')
+      print('Still bigger')
+      #the scope of this block ends here
+print('Done with 2')
+
+for i in range(5)  :
+      print(i)
+      if i > 2 :
+          print('Bigger than 2')
+      print('Done with i', i)
+print('All Done')
